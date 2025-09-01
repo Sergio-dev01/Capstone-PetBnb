bd2dee516e9bdf96c4881ec0548b002009f115d0
Sergio-dev01 <sergio.develoop@gmail.com>
2025-09-01 17:25:22 +0200

    Initial commit

---
+++ Backend/src/main/java/com/mypetbnb/petbnb/PetbnbApplication.java
@@ -0,0 +1,13 @@
+package com.mypetbnb.petbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetbnbApplication.class, args);
+	}
+
+}
