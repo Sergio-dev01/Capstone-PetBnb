e1aeffdf8c8abd64dedd5dc7be963a9a6fbd98b1
Sergio-dev01 <sergio.develoop@gmail.com>
2025-09-04 16:10:55 +0200

    create base frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import WelcomePage from "./components/WelcomePage";
+import RegisterPage from "./components/RegisterPage";
+import LoginPage from "./components/LoginPage";
+import UserPage from "./components/UserPage";
+import LocationPage from "./components/LocationPage";
+import BookingPage from "./components/BookingPage";
+import HostBookingsPage from "./components/HostBookingPage";
+import AddLocationPage from "./components/AddLocationPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/users/me" element={<UserPage />} />
+          <Route path="/locations" element={<LocationPage />} />
+          <Route path="/bookings" element={<BookingPage />} />
+          <Route path="/host/bookings" element={<HostBookingsPage />} />
+          <Route path="/locations/add" element={<AddLocationPage />} />
+          <Route path="*" element={<div>Pagina non trovata</div>} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
