6ddeb61682ea156727468b0254c3a4601ff324c2
Sergio-dev01 <sergio.develoop@gmail.com>
2025-09-05 17:27:50 +0200

    add mod on auth controller and securityConfig

--- Backend/src/main/java/com/mypetbnb/petbnb/controllers/AuthController.java
+++ Backend/src/main/java/com/mypetbnb/petbnb/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.mypetbnb.petbnb.controllers;
 
 import com.mypetbnb.petbnb.entities.User;
+import com.mypetbnb.petbnb.exceptions.DuplicateEmailException;
 import com.mypetbnb.petbnb.exceptions.ValidationException;
 import com.mypetbnb.petbnb.payload.LoginRespDTO;
 import com.mypetbnb.petbnb.payload.NewUserDTO;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,14 +34,20 @@ public class AuthController {
     @ResponseStatus(HttpStatus.CREATED)
     public UserRespDTO register(@RequestBody @Validated NewUserDTO body, BindingResult validation) {
         if (validation.hasErrors()) {
-            throw new ValidationException(validation.getFieldErrors().stream().map(fieldError -> fieldError.getDefaultMessage()).toList());
-
-        } else {
-            User newUser = this.userService.createUser(body);
-            return new UserRespDTO(newUser.getId());
+            throw new ValidationException(validation.getFieldErrors().stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList());
         }
 
+
+        if (userService.existsByEmail(body.email())) {
+            throw new DuplicateEmailException("Email già registrata");
+        }
+
+        User newUser = userService.createUser(body);
+        return new UserRespDTO(newUser.getId());
     }
+
 
     @PostMapping("/login")
     public LoginRespDTO login(@RequestBody UserLoginDTO payload) {
